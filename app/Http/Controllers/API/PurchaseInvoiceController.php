bfabb70469f7a37a447549a2c7581ad1bf8c7bd6
Abdullah Arif <abdullah.arif@qbatch.com>
2022-05-01 02:24:13 +0500

    update

--- app/Http/Controllers/API/PurchaseInvoiceController.php
+++ app/Http/Controllers/API/PurchaseInvoiceController.php
@@ -10,6 +10,7 @@ use App\Models\SellCostPrice;
 use App\Models\Transaction;
 use Validator;
 use DB;
+use App;
 use App\Http\Resources\PurchaseInvoice as PurchaseInvoiceResource;
 use App\Http\Resources\PurchaseOrder as PurchaseOrderResource;
 use App\Http\Resources\PurchaseReport as ReportResource;
@@ -75,13 +76,41 @@ class PurchaseInvoiceController extends BaseController
     }
     public function send_invoice_email(Request $request){
         $input = $request->all();
+
         $message = $input['invoice_message'];
+
+        $pdf = App::make('dompdf.wrapper');
+        $pdf->loadHTML($message);
+        $invoice = $pdf->stream('invoice.pdf');
+        $content = chunk_split(base64_encode($invoice));
+
+        $separator = md5(time());
+        $eol = "\r\n";
+
         $to = $input['email'] ? $input['email'] : "abdullaharif789@gmail.com";
         $subject="Invoice from EveryDayFreshFood";
         $from = "invoices@everydayfreshfood.com";
-        $headers = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $headers .= "From: $from";
+
+        $headers = "From: EverDay Fresh Food Invoice ". $from . $eol;
+        $headers .= "MIME-Version: 1.0" . $eol;
+        $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
+        $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
+        $headers .= "This is a MIME encoded message." . $eol;
+
+        // message
+        $body = "--" . $separator . $eol;
+        $body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
+        $body .= "Content-Transfer-Encoding: 8bit" . $eol;
+        $body .= $message . $eol;
+
+        // attachment
+        $body .= "--" . $separator . $eol;
+        $body .= "Content-Type: application/octet-stream; name=\"" . "invoice.pdf" . "\"" . $eol;
+        $body .= "Content-Transfer-Encoding: base64" . $eol;
+        $body .= "Content-Disposition: attachment" . $eol;
+        $body .= $content . $eol;
+        $body .= "--" . $separator . "--";
+
         if(mail($to,$subject,$message,$headers)){
             return $this->sendResponse('Invoice sent successfully.',null);
         }
