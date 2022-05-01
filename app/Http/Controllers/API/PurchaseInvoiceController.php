940a6a1a8d118b20dfdb778caedcf7c0b1774400
Abdullah Arif <abdullah.arif@qbatch.com>
2022-05-01 05:40:19 +0500

    update

--- app/Http/Controllers/API/PurchaseInvoiceController.php
+++ app/Http/Controllers/API/PurchaseInvoiceController.php
@@ -14,7 +14,8 @@ use App;
 use App\Http\Resources\PurchaseInvoice as PurchaseInvoiceResource;
 use App\Http\Resources\PurchaseOrder as PurchaseOrderResource;
 use App\Http\Resources\PurchaseReport as ReportResource;
-
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 class PurchaseInvoiceController extends BaseController
 {
     /**
@@ -75,50 +76,32 @@ class PurchaseInvoiceController extends BaseController
         return $this->sendResponse(PurchaseInvoiceResource::collection($purchaseInvoices->get()), 'Purchase Invoices retrieved successfully.',$count);
     }
     public function send_invoice_email(Request $request){
-        $input = $request->all();
-
-        $message = "Body Message";
-
+        $body = "You can download invoice from attachment.";
+        $message = $request->invoice_message;
+        $invoice_path = date('d_m_Y_His').".pdf";
         $pdf = App::make('dompdf.wrapper');
-        $pdf->loadHTML($message);
-        $invoice = $pdf->stream('invoice.pdf');
-        $content = chunk_split(base64_encode("1234"));
+        $pdf->loadHTML($message)->save($invoice_path);
 
-        $separator = md5(time());
-        $eol = "\r\n";
+        $mail = new PHPMailer(true);
 
-        $to = $input['email'] ? $input['email'] : "abdullaharif789@gmail.com";
-        $subject="Invoice from EveryDayFreshFood";
-        $from = "invoices@everydayfreshfood.com";
+        $mail->setFrom('invoice@everydayfreshfood.com', 'EveryDay Fresh Food');
+        $mail->addAddress($request->email);
+        $mail->addAddress("everydayfreshfood007@gmail.com");
 
-        $headers = "From: EverDay Fresh Food Invoice ". $from . $eol;
-        $headers .= "MIME-Version: 1.0" . $eol;
-        $headers .= "Content-Type: multipart/mixed; boundary=\"" . $separator . "\"" . $eol;
-        $headers .= "Content-Transfer-Encoding: 7bit" . $eol;
-        $headers .= "This is a MIME encoded message." . $eol;
+        $mail->AddAttachment($invoice_path,"invoice_".$invoice_path);
 
-        // message
-        $body = "--" . $separator . $eol;
-        $body .= "Content-Type: text/plain; charset=\"iso-8859-1\"" . $eol;
-        $body .= "Content-Transfer-Encoding: 8bit" . $eol;
-        $body .= $message . $eol;
+        $mail->isHTML(true);
 
-        // attachment
-        $body .= "--" . $separator . $eol;
-        $body .= "Content-Type: application/octet-stream; name=\"" . "invoice.pdf" . "\"" . $eol;
-        $body .= "Content-Transfer-Encoding: base64" . $eol;
-        $body .= "Content-Disposition: attachment" . $eol;
-        $body .= $content . $eol;
-        $body .= "--" . $separator . "--";
+        $mail->Subject = "Invoice - EveryDayFreshFood";
+        $mail->Body    = $body;
 
-        $response=mail($to,$subject,$body,$headers);
-        if($response){
-            return $this->sendResponse('Invoice sent successfully.',null);
+        if($mail->send()) {
+             return $this->sendResponse('Invoice sent successfully.',null);
         }
-        else{
-            print_r( error_get_last() );
+        else {
             return $this->sendResponse('Sorry, Invoice sent unsuccessfully. Please try later.',null);
         }
+        unlink($invoice_path);
     }
     public function revised_purchase_orders(Request $request)
     {
